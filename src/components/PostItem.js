515f56bad36cf43bb5bc031fa8388d6e2e493ae7
Dione <dioneg12@hotmial.com>
2020-09-19 12:42:09 -0300

    memo exemplo

---
+++ src/components/PostItem.js
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+
+const PostItem = ({ post }) => {
+  return (
+    <li>
+      <strong>{post.title}</strong>
+      <p>{post.body}</p>
+    </li>
+  );
+};
+
+export default memo(PostItem);
